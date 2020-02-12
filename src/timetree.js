cef2b1e18d249f835b41dd695a7ec92f40135435
hagiwara <hagiwara-ky@rittor-music.co.jp>
2020-02-12 16:58:36 +0900

    ttのメソッドをimplement

--- src/timetree.js
+++ src/timetree.js
@@ -1,63 +1,8 @@
 var scriptProperties = PropertiesService.getScriptProperties();
 var targetCalendarId = scriptProperties.getProperty('TARGET_CALENDAR_ID_DEV');
 
-// テスト用メソッド
-function timtreeTest() {
-  // var eventOptions =
-  //   var resres = timeTreeCreateEvent(eventOptions);
-  Logger.log(resres); // ユーザー取得メソッドをコール
-  add_row();
-}
-
-// カレンダー取得メソッド
-function timetreeGetUser() {
-  var url = 'https://timetreeapis.com/calendars';
-  var method = 'GET';
-  var payload = '';
-  return timetreeAPI(url, method, payload); // TimeTree APIをコール
-}
 
 // イベント作成メソッド
-function timetreePostEvent() {
-  //  Logger.log("kita");
-
-
-  var url = Utilities.formatString('https://timetreeapis.com/calendars/%s/events', targetCalendarId);
-  var method = 'POST';
-  var payload = {
-    'data': {
-      'attributes': {
-        'category': 'schedule',
-        'title': 'てすとおおおおおおいべんとお',
-        'all_day': false,
-        'start_at': '2019-03-18T09:00:00.000Z',
-        'start_timezone': 'UTC',
-        'end_at': '2019-03-18T10:00:00.000Z',
-        'end_timezone': 'UTC',
-        'description': 'blah blah blah',
-        'location': 'Tokyo',
-        'url': 'https://example.com'
-      },
-      'relationships': {
-        'label': {
-          'data': {
-            'id': 'uIW4Wt9M8sVD,1',
-            'type': 'label'
-          }
-        }
-      }
-    }
-  };
-
-  return timetreeAPI(url, method, payload); // TimeTree APIをコール
-}
-
-
-function timeTreeDeleteEventById(eventId) {
-  return 0;
-}
-
-
 function timeTreeCreateEvent(eventOptions) {
   var url = Utilities.formatString('https://timetreeapis.com/calendars/%s/events', targetCalendarId);
   var method = 'POST';
@@ -88,10 +33,45 @@ function timeTreeCreateEvent(eventOptions) {
   return timetreeAPI(url, method, payload); // TimeTree APIをコール
 }
 
-
+// イベント更新メソッド
 function timeTreeUpdateEventById(eventId, eventOptions) {
-  return 0;
+  var url = Utilities.formatString('https://timetreeapis.com/calendars/%s/events/%s', targetCalendarId, eventId);
+  var method = 'PUT';
+  var payload = {
+    'data': {
+      'attributes': {
+        'category': 'schedule',
+        'title': eventOptions.title,
+        'all_day': eventOptions.isAllDay,
+        'start_at': eventOptions.start,
+        'start_timezone': 'UTC',
+        'end_at': eventOptions.end,
+        'end_timezone': 'UTC',
+        'description': eventOptions.description,
+        'location': eventOptions.location,
+      },
+      'relationships': {
+        'label': {
+          'data': {
+            'id': 'uIW4Wt9M8sVD,1',
+            'type': 'label'
+          }
+        }
+      }
+    }
+  };
+
+  return timetreeAPI(url, method, payload); // TimeTree APIをコール
 }
+
+// イベント削除メソッド
+function timeTreeDeleteEventById(eventId) {
+  var url = Utilities.formatString('https://timetreeapis.com/calendars/%s/events/%s', targetCalendarId, eventId);
+  var method = 'DELETE';
+  var payload = '';
+  return timetreeAPI(url, method, payload); // TimeTree APIをコール
+}
+
 
 
 // TimeTree APIをコールするメソッド
@@ -113,13 +93,54 @@ function timetreeAPI(url, method, payload) {
   return response;
 }
 
-function gcal_test() {
-  var cal = CalendarApp.getCalendarById(scriptProperties.getProperty('google_email_address'));
-  var date = new Date();
-  var events = cal.getEventsForDay(date);
-  Logger.log(events[0].devareEvent());
+
+
+
+// テスト用メソッド
+function timtreeTest() {
+  // var eventOptions =
+  //   var resres = timeTreeCreateEvent(eventOptions);
+  Logger.log(resres); // ユーザー取得メソッドをコール
+  add_row();
 }
 
-function trigger_calender_updated() {
-  var sabun = 0;
+// カレンダー取得メソッド
+function timetreeGetUser() {
+  var url = 'https://timetreeapis.com/calendars';
+  var method = 'GET';
+  var payload = '';
+  return timetreeAPI(url, method, payload); // TimeTree APIをコール
+}
+
+// テストイベント作成メソッド
+function timetreePostEventTest() {
+
+  var url = Utilities.formatString('https://timetreeapis.com/calendars/%s/events', targetCalendarId);
+  var method = 'POST';
+  var payload = {
+    'data': {
+      'attributes': {
+        'category': 'schedule',
+        'title': 'てすとおおおおおおいべんとお',
+        'all_day': false,
+        'start_at': '2019-03-18T09:00:00.000Z',
+        'start_timezone': 'UTC',
+        'end_at': '2019-03-18T10:00:00.000Z',
+        'end_timezone': 'UTC',
+        'description': 'blah blah blah',
+        'location': 'Tokyo',
+        'url': 'https://example.com'
+      },
+      'relationships': {
+        'label': {
+          'data': {
+            'id': 'uIW4Wt9M8sVD,1',
+            'type': 'label'
+          }
+        }
+      }
+    }
+  };
+
+  return timetreeAPI(url, method, payload); // TimeTree APIをコール
 }
